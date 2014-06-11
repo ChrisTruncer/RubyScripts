df3eeaa48ed0479f935eeb6f02f7e3bf0fc53a3f
Christopher Truncer <chris@christophertruncer.com>
2014-06-11 19:32:02 -0400

    Initial commit, with nmap xml parser

---
+++ learning/nmapxml.rb
@@ -0,0 +1,162 @@
+#!/usr/bin/env ruby
+
+require 'nokogiri'
+
+class NmapParser < Nokogiri::XML::SAX::Document
+  
+  def initialize
+    @ip_address = nil
+    @hostname = nil
+    @potential_port = nil
+    @final_port_number = nil
+    @port_state = nil
+    @protocol = nil
+    @tunnel = nil
+    @final_url = nil
+    @url_array = []
+  end
+
+  def start_element name, attrs = []
+    @attrs = attrs
+
+    # Find IP addresses of all machines
+    if name == "address"
+      @attrs.each do |key, value|
+        if key == "addr"
+          @ip_address = value
+        end
+      end
+    end
+
+    if name == "hostname"
+      @hostname = nil
+      @attrs.each do |key, value|
+        if key == "name"
+          @hostname = value
+        end
+      end
+    end
+    
+    if name == "port"
+      @attrs.each do |key, value|
+        if key == "portid"
+          @potential_port = value
+        end
+      end
+    end
+
+    # Find port state
+    if name == "state"
+      @attrs.each do |key, value|
+        if key == "state"
+          if value == "open"
+            @port_state = "open"
+          else
+            @port_state = "closed"
+          end
+        end
+      end
+    end
+
+    # Find port "name"
+    if name == "service"
+      @attrs.each do |key, value|
+        if key == "name"
+          if value.include? "https"
+            @protocol = "https://"
+            @final_port_number = @potential_port
+
+          elsif value.include? "http"
+            @protocol = "http://"
+            @final_port_number = @potential_port
+          end
+        end
+
+        if key == "tunnel"
+          if value.include? "ssl"
+            @tunnel = "ssl"
+          end
+        end
+      end   # end attrs iterator
+
+      if @protocol == "https://" || @tunnel == "ssl"
+        @protocol = "https://"
+        if @hostname.nil? && @port_state == "open"
+          @final_url = "#{@protocol}#{@ip_address}:#{@final_port_number}"
+          if !@url_array.include? @final_url
+            @url_array << @final_url
+          else
+          end
+          
+        elsif @port_state == "open"
+          @final_url = "#{@protocol}#{@hostname}:#{@final_port_number}"
+          if !@url_array.include? @final_url
+            @url_array << @final_url
+          else
+            @final_url = "#{@protocol}#{@ip_address}:#{@final_port_number}"
+            if !@url_array.include? @final_url
+            @url_array << @final_url
+            else
+            end
+          end
+        else
+        end
+
+      elsif @protocol == "http://"
+        if @hostname.nil? && @port_state == "open"
+          @final_url = "#{@protocol}#{@ip_address}:#{@final_port_number}"
+          if !@url_array.include? @final_url
+            @url_array << @final_url
+          else
+          end
+        elsif @port_state == "open"
+          @final_url = "#{@protocol}#{@hostname}:#{@final_port_number}"
+          if !@url_array.include? @final_url
+            @url_array << @final_url
+          else
+            @final_url = "#{@protocol}#{@ip_address}:#{@final_port_number}"
+            if !@url_array.include? @final_url
+            @url_array << @final_url
+            else
+            end
+          end
+        else
+        end   #End of if statement printing valid servers
+      end    # End if statement looking at protocol and tunnel
+    end    # End of if statement for the element starting with the name "service"
+  end    # End of start_element function
+
+
+  def end_element name
+    if name == "host"
+      @ip_address = nil
+      @hostname = nil
+    end
+
+    if name == "service"
+      @potential_port = nil
+      @final_port_number = nil
+      @port_state = nil
+      @protocol = nil
+      @tunnel = nil
+      @final_url = nil
+    end
+  end   # End of end_element function
+
+  def url_get
+    @url_array
+  end
+
+end   # End of nmap parsing class
+
+nmap_class = NmapParser.new
+
+parser = Nokogiri::XML::SAX::Parser.new(nmap_class)
+
+parser.parse(File.open(ARGV[0]))
+
+all_urls = nmap_class.url_get
+
+all_urls.each do |url|
+  puts url
+end
